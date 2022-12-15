ea49761d9fbadcfa7938af2638ebaf9c24cd8c21
Alistair Haslam <AlistairHaslam@googlemail.com>
2022-12-15 10:08:54 +0000

    Changed to listen to the https endpoint.

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quizapiamh-env.eba-pkfahdt3.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
